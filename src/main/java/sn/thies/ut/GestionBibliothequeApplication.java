333c167dcd3d80f9d3b1c6b7eca17dcadd935254
thiamzi <thiamzirabm@gmail.com>
2021-04-12 05:44:14 +0200

    edit app.java

--- src/main/java/sn/thies/ut/GestionBibliothequeApplication.java
+++ src/main/java/sn/thies/ut/GestionBibliothequeApplication.java
@@ -26,7 +26,7 @@ public class GestionBibliothequeApplication {
 
 	corsConfiguration.setAllowCredentials(true);
 
-	corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+	corsConfiguration.setAllowedOrigins(Arrays.asList("https://biblioteque-4dc77.web.app"));
 
 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 
