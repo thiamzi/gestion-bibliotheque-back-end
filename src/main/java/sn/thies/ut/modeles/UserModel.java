327f174a977df62454e3da6979b09c6c32559bb7
thiamzi <thiamzirabm@gmail.com>
2021-04-24 04:29:57 +0200

    edit user model

--- src/main/java/sn/thies/ut/modeles/UserModel.java
+++ src/main/java/sn/thies/ut/modeles/UserModel.java
@@ -24,7 +24,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author PC
  */
 @Entity
-@Table(name = "user")
+@Table(name = "usermodel")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "User.findAll", query = "SELECT u FROM UserModel u")
