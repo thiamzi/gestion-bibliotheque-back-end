bf61074e6e3a8a20abc90dc19809b559040f15cd
thiamzi <thiamzirabm@gmail.com>
2021-05-29 14:03:02 +0200

    add swagger

--- src/main/java/sn/thies/ut/GestionBibliothequeApplication.java
+++ src/main/java/sn/thies/ut/GestionBibliothequeApplication.java
@@ -9,14 +9,29 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import email.EmailService;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class GestionBibliothequeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionBibliothequeApplication.class, args);
 		
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("sn.thies.ut"))
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 	@Bean
