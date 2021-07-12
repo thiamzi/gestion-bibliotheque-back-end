306ecc7fc3c3b15db204771ded6bef6b138d95bc
René Augustin Mbengue <48734902+thiamzi@users.noreply.github.com>
2021-07-12 21:17:46 +0200

    Update GestionBibliothequeApplication.java

--- src/main/java/sn/thies/ut/GestionBibliothequeApplication.java
+++ src/main/java/sn/thies/ut/GestionBibliothequeApplication.java
@@ -26,7 +26,7 @@ public class GestionBibliothequeApplication {
 
 	corsConfiguration.setAllowCredentials(true);
 	
-	corsConfiguration.setAllowedOrigins(Arrays.asList("https://biblioteque-4dc77.web.app"));
+	corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 
 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 
